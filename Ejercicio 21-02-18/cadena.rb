1cec89acb65ac88c02d2596592a31e6d371d7a57
capaisa12 <capaisa12@gmail.com>
2018-02-21 06:54:33 -0500

    Clase del 21 de Febrero

---
+++ Ejercicio 21-02-18/cadena.rb
@@ -0,0 +1,15 @@
+#Escribe un programa que le pregunte al usuario por una cadena de texto y despues la imprima en mayusculas e invertida
+
+#def Mayusculas(cadena)
+#  cadena.upcase
+#end
+
+#def Invierte(cadena)
+#   cadena.reverse
+ #end
+
+
+print "Ingrese una cadena de texto: "
+cadena = gets.chomp
+puts cadena.upcase
+puts cadena.reverse
